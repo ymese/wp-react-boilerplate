c4137b955a8419bc0b9535886b25712ddc989078
Thinh Nguyen <thinhnp@ymese.com>
2018-01-20 14:37:44 +0700

    Add example for building

---
+++ index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>This is WordPress React Boilerplate</div>
+        )
+    }
+}
